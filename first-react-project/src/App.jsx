f864da72012a290d0b64421109a99dff284dacf6
janvi7114 <chichuddejanvi@gmail.com>
2025-11-24 23:04:29 +0530

    deleted first project file and created it inside single folder

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+
+
+  return (
+
+      <div>
+      <p>
+        project name should be all small letters no space no caps no underscore
+        this return should be only single parent div tag.
+      </p>
+    </div>
+  )
+
+
+export default App
