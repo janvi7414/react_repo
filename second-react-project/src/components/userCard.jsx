786b4db72c91317d18ba58ac75fab8d59eb53951
janvi7114 <chichuddejanvi@gmail.com>
2025-11-24 23:31:12 +0530

    added profile images

---
+++ second-react-project/src/components/userCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import profile1 from '../assets/profile1.png'
+
+const userCard = () => {
+  return (
+    <div>
+      <p id="user_name" >Janvi</p>
+      <img id="user_img" src={profile1} alt="i" />
+      <p id="user_desc">Description of the tile</p>
+    </div>
+  )
+}
+
+export default userCard
+
+// here instead of class className is used
+/*
+naming project name using terminal
+  ✔ all lowercase
+  ✔ numbers allowed
+  ✔ hyphens allowed
+  ✔ NO underscores
+  ✔ NO uppercase
+  ✔ NO spaces
+naming file name in src
+  ✔ lowercase or uppercase
+  ✔ hyphens (-)
+  ✔ underscores (_)
+  ✔ numbers
+  ✔ CamelCase reactCode (recommended for React components)
+*/
