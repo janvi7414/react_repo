b6a01979ef018056ead0efdfbaa6874d8b8aa307
janvi7114 <chichuddejanvi@gmail.com>
2025-11-29 22:13:19 +0530

    added comment spaces

--- use-effect/use-effect/src/App.jsx
+++ use-effect/use-effect/src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   const [total,setTotal] = useState(10);
 
   //variation 1: runs on every render
-  // useEffect(()=>{
+  // useEffect(() => {
   //   alert("runs on every render");
   //   //this will run twice due to <StrictMode/> in main.jsx
   // })
 
 
   //variation 2: runs on first render only
-  // useEffect(()=> {
+  // useEffect(() => {
   //   alert("this will run on first render only");
   //   //here empty dependencies list is passed
   // }, [] )
@@ -21,7 +21,7 @@ function App() {
 
   // variation 3: runs every time when there is update in one of the dependencies passed
 
-  // useEffect(()=>{
+  // useEffect(() => {
   //   alert("this will run on first render and whereever the count updates");
   //   //it is not applied on updation of total var
   //   //it also runs for initial value set for the count
@@ -30,12 +30,12 @@ function App() {
 
 
   //variation 4: runs for multiple dependencies
-  //   useEffect(()=>{
+  //   useEffect(() => {
   //   alert("this will run on first render and whereever the count/total updates");
   // },[count,total])
 
   //variation 5: adding cleanup function
-  useEffect(()=>{
+  useEffect(() => {
     
     //this runs before the value of count is updated 
     alert("this will run for first render and also when count is updated")
