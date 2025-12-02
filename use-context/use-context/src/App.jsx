dbc4fec4edfe456e8db1af8951dd5417979c37fd
janvi7114 <chichuddejanvi@gmail.com>
2025-12-02 19:06:56 +0530

    added comment

--- use-context/use-context/src/App.jsx
+++ use-context/use-context/src/App.jsx
@@ -34,6 +34,8 @@ export { myContext }
     
     */
    /* prop drilling: usually when there are multiple heirachical children to a parent when any deeper child wants to access the data form parent in between nodes need to unncessarily read the data thus useContext helps to  make parent provider and the child consumer only desired child becomes consumer to the data
+
+   React Context provides a way to share data across the component tree without manually passing props down through every level, a practice known as "prop drilling." It is particularly useful for managing "global" state or data that needs to be accessible by many components at different nesting levels, such as themes, user authentication status, or language preferences. 
    
    
    */
