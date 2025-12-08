24cabafc669a7dbfdb04678c9ec4acb9ed3e94c9
janvi7114 <chichuddejanvi@gmail.com>
2025-12-08 18:48:12 +0530

    added thunk theory in comments

--- redux-toolkit/redux-toolkit/src/redux/store.js
+++ redux-toolkit/redux-toolkit/src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore , combineReducers} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
@@ -12,9 +12,36 @@ export const store = configureStore({
 setupListeners(store.dispatch)
 
 /*
+
+RTK: redux toolkit
 step:
     create store
     wrap app component under Provider
     create slice
     register reducer in store
+ */
+
+
+/*
+A thunk is a function that allows you to write asynchronous logic (API calls, timers, database calls, etc.) in Redux.
+Normally, Redux reducers must be pure and synchronous.
+But real applications need async operations (fetch data from backend, login, etc.).
+Thunk lets Redux handle async code.
+
+Redux Toolkit includes redux-thunk by default — you don’t need to install it or add middleware.
+So you can immediately write async functions using:createAsyncThunk()
+
+Redux only handles synchronous updates, so we need thunks to handle async tasks like:
+    Fetching data from an API
+    Logging in a user
+    Posting data to a server
+    Delaying an action
+    ing multiple dispatches in order
+
+A thunk is basically a function that:
+    Returns another function
+    That inner function receives dispatch and getState
+    You can run async code inside it
+    When done, you dispatch normal actions
+
  */
