ebdb46abd07f7649e74bb193528f5b7f3bdb758b
janvi7114 <chichuddejanvi@gmail.com>
2025-12-12 14:14:30 +0530

    addedt theory commit

--- use-effect-example/use-effect-example/src/components/DataFetcher.jsx
+++ use-effect-example/use-effect-example/src/components/DataFetcher.jsx
@@ -37,3 +37,44 @@ const DataFetcher = () => {
 export default DataFetcher
 
 //to make loading visible set the network to lower version 
+
+/*Code      Part	Why Used?	    Datatype
+  fetch()	  ---	Function --- returning a Promise
+  .then()	--- Handle Promise result	--- Method that takes a function
+  response => ...	--- Small callback function	--- Function
+  response.json()	--- Convert to JSON	--- Promise
+  data => {...}	--- Handle JSON	--- Function
+  setData(data)	--- Save API data	--- Function call
+  setLoading(false)	--- Stop loading	--- Function call
+  false	--- Boolean value	--- Boolean
+
+
+  JavaScript lets you create functions in 3 main ways:
+    function myFunc() {},
+    const myFunc = function() {},
+    const myFunc = () => {}.
+
+    map() is a method used on arrays
+          array.map((item, index) => {
+      // return something
+    });
+
+    item = current element
+    index = (optional) position of element
+
+    ✔ map() always returns a new array
+    ✔ Original array stays the same
+    ✔ You must return something inside map
+    ✔ In React, each mapped element needs a key
+
+    | Feature            | Normal Function       | Arrow Function              |
+| ------------------ | --------------------- | --------------------------- |
+| Syntax             | Longer                | Shorter                     |
+| Own `this`         | ✔ Yes                 | ❌ No                        |
+| Can be constructor | ✔ Yes                 | ❌ No                        |
+| Has `arguments`    | ✔ Yes                 | ❌ No                        |
+| Hoisted            | ✔ Yes                 | ❌ No                        |
+| Best for           | Methods, constructors | Callbacks, React components |
+
+  
+    */
