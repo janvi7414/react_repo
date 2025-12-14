e0fb8bba4ce46612cc6f6fefbd21aa96a38d8d76
janvi7114 <chichuddejanvi@gmail.com>
2025-12-15 01:02:48 +0530

    added text area

--- notes-saver-project/notes-saver-project/src/components/Home.jsx
+++ notes-saver-project/notes-saver-project/src/components/Home.jsx
@@ -7,22 +7,34 @@ const Home = () => {
   const [title, setTitle] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   const pasteId = searchParams.get("pasteId");
+  const [value, setValue] = useState('');
 
 
   return (
+    <div>
     <div className='flex flex-row justify-between gap-1.5'>
      <input
       placeholder='Enter title'
       value={title}
       onChange={(e) => setTitle(e.target.value)}
-      className='flex flex-row justify-start p-1 w-2/3 rounded-sm'
+      className='flex flex-row justify-start p-1 w-3/4 rounded-sm border-2 focus:border-blue-400 focus:outline-none '
      />
 
-     <button className=' rounded-sm border w-1/3 font-medium'>
+     <button className=' border w-1/4 font-medium bg-gray-400 text-white rounded-xl p-2 focus:bg-gray-600'>
       {/* the text on button depends 
           create my paste -> when user clicks it from home i.e. url "/" and update my paste -> when user clicked it after visiting pastes i.e. list of paste thus url contains let pasteId "localhost:5173/?pasteId=abc" thus we are using searchParams to get the pasteId  */}
           {pasteId ? "Update My Paste" : "Create My Paste"}
      </button>
+     </div>
+     <div className='bg-gray-200 mt-4 p-3 text-left rounded-md'>
+      <textarea 
+        className='w-full focus:outline-0'
+        placeholder='Enter Content'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        rows={20}
+      />
+     </div>
     </div>
   )
 }
